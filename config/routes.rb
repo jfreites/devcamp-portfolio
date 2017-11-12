df90a98a290c21da5a0e81fb8af517569ccdc730
Jonathan Freites <jonathanfreites@gmail.com>
2017-11-11 19:23:19 -0600

    Added status toggle funcionality for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
+    resources :portfolios, except: [:show]
+    get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
   
-  get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+    get 'about-me', to: 'pages#about'
+    get 'contact', to: 'pages#contact'
 
-  resources :blogs
+    resources :blogs do
+        member  do
+            get :toggle_status
+        end
+    end
   
-  root to: 'pages#home'
+    root to: 'pages#home'
 end
